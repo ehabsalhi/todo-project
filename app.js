9585d53a8198d23c85d1d18337ef7add45b4bf51
ehabsalhi <ehab1salhe@gmail.com>
2023-04-11 13:57:37 +0000

    solving lab 6

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-let username = prompt("Enter your Name Please ");
-let gender = prompt("Choose your gender Please","male or female");
-let age = prompt("Enter your Age Please")
+ let username = prompt("Enter your Name Please ");
+ let gender = prompt("Choose your gender Please","male or female");
+ let age = prompt("Enter your Age Please")
 
-if( age <= 0){
-     alert("Not allowed to enter because of your age")
+ if( age <= 0){
+      alert("Not allowed to enter because of your age")
+ }
+
+ let conf = confirm("do you want to skip the welcoming message ?")
+
+ if(conf == false){
+      if(gender == "male"){
+
+           alert(`hello Mr ${username} `)
+      }
+      else if(gender == "female"){
+           alert(`hello Ms ${username} `)
+      }
+      else{
+           alert(`hello ${username} `)
+
+      }
+ }
+
+ // lab 6 
+
+let q1 = prompt('do you have a job' , 'Yes Or No')
+let q3 = prompt('are you married ', 'Yes Or No')
+let q2 = prompt('do you have children', 'Yes Or No')
+
+let answerArray = []
+
+
+function checkAnswer(answer){
+
+     if (answer  === ''){
+          answer = 'invalid'
+     }
+
+     answerArray.push(answer)
+     return answerArray
 }
 
-let conf = confirm("do you want to skip the welcoming message ?")
+checkAnswer(username)
+checkAnswer(gender)
+checkAnswer(username)
+checkAnswer(q1)
+checkAnswer(q2)
+checkAnswer(q3)
 
-if(conf == false){
-     if(gender == "male"){
-
-          alert(`hello Mr ${username} `)
-     }
-     else if(gender == "female"){
-          alert(`hello Ms ${username} `)
-     }
-     else{
-          alert(`hello ${username} `)
-
-     }
-}
-
+console.log( answerArray)
